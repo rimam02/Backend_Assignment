e1d9fa287cf7828fb29ffa4f0d8373c5a04e722d
Rima Amal Maji <2025.rimam@isu.ac.in>
2026-09-23 10:55:18 +0530

    Implement a simple HTTP server for student data

---
+++ Asst_2/question3/index.js
@@ -0,0 +1,23 @@
+const http = require("http");
+
+const myServer = http.createServer((req, res) => {
+    if (req.url === "/student") {
+        const student = {
+            id: 101,
+            name: "John",
+            course: "BCA",
+            semester: 4,
+            city: "Mumbai"
+        };
+
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(student));
+    } else {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("404 - Page Not Found");
+    }
+});
+
+myServer.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
