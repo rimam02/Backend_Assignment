b1f8abfb11781841dd118d3615b4dce6bff49532
Rima Amal Maji <2025.rimam@isu.ac.in>
2026-09-25 12:00:48 +0530

    Add teacher schema for MongoDB using Mongoose

---
+++ Asst_11/schema/teacherSchema.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const teacherSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+
+    subject: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = teacherSchema;
